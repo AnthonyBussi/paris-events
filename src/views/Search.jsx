b4ebc025542a5f5759b05c59bc76c575a9cdaf1b
Anthony Bussi <anthony.bussi@gmail.com>
2021-09-10 12:13:31 +0200

    Add modal on search

--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import Card from '../components/Card';
+import CardDetails from '../components/CardDetails';
 import './Search.css';
 
 function Search() {
@@ -8,19 +9,38 @@ function Search() {
     const [datas, setDatas] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
 
+    const [event_, setEvent_] = useState(null);
+    const [event_id, setEvent_id] = useState(null);
+    const [displayModal, setDisplayModal] = useState(false);
+
     const URL = "https://opendata.paris.fr/api/v2/catalog/datasets/que-faire-a-paris-/records?limit=12&search=";
+
 
     const handleSearchTerm = (e) => {
         let value = e.target.value;
         setSearchTerm(value);
-    }    
-    
+    }
+
 
     useEffect(() => {
-        fetch(URL+searchTerm)
+        fetch(URL + searchTerm)
             .then(response => response.json())
-            .then(result => setDatas(result.records))
+            .then(result => {
+                setDatas(result.records)
+                const event_id_ = result.records[0].record.id;
+                const event_ = result.records[0].record.fields;
+                setEvent_id(event_id_);
+                setEvent_(event_);
+            })
     }, [searchTerm]); // fin useEffect
+
+    const openModal = () => {
+        setDisplayModal(true);
+    }
+
+    const closeModal = () => {
+        setDisplayModal(false);
+    }
 
     // console.log(datas);
 
@@ -40,9 +60,11 @@ function Search() {
                         <button type="button" name="search-btn" className="search-btn">Rechercher</button>
                     </form>
                     <div className="search-result-container">
-                        { datas && datas.map((event) => (
-                            <Card key={ event.record.id } id={ event.record.id } dataEvent={ event.record.fields } />
+                        {datas && datas.map((event) => (
+                            <Card key={event.record.id} id={event.record.id} dataEvent={event.record.fields} onOpenModal={openModal} />
                         ))}
+
+                        {displayModal && <CardDetails dataEvent={event_id, event_} onCloseModal={closeModal} />}
                     </div>
                 </div>
             </main>
